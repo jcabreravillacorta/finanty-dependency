98f643f4864ef4b966ec6c410af394943b526ad5
dpinedac <dpineda@finanty.com>
2023-10-04 23:07:37 -0500

    update

--- src/main/java/pe/finanty/servDepenFinanty/LocalDateUtils.java
+++ src/main/java/pe/finanty/servDepenFinanty/LocalDateUtils.java
@@ -3,6 +3,7 @@ package pe.finanty.servDepenFinanty;
 import lombok.extern.slf4j.Slf4j;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 @Slf4j
@@ -30,5 +31,18 @@ public class LocalDateUtils {
 
         DateTimeFormatter formateador = DateTimeFormatter.ofPattern(formatter);
         return LocalDate.parse(fechaStr, formateador);
+    }
+
+    /**
+     * Método que convierte un LocalDate a un String
+     *
+     * @param fecha ejemplo "06/09/2023".
+     * @param formatter  En formato ejemplo dd/MM/yyyy.
+     * @return retorna un LocalDate.
+     */
+    public static String convertLocalDateToStringFormatter(LocalDate fecha, String formatter){
+
+        DateTimeFormatter formateador = DateTimeFormatter.ofPattern(formatter);
+        return fecha.format(formateador);
     }
 }
